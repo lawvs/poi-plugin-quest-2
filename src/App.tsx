76647577b8805b684bca33386cf6ec693f80709d
lawvs <18554747+lawvs@users.noreply.github.com>
2021-01-30 01:56:43 +0900

    feat: add search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+import { InputGroup } from '@blueprintjs/core'
+import { IconNames } from '@blueprintjs/icons'
 import styled from 'styled-components'
+import { ChangeEvent, StrictMode, useCallback, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { QuestCard } from './components/QuestCard'
 import QuestData from '../build/kcanotifyGamedata'
+import { useThrottle } from './utils'
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  overflow: hidden;
+
+  & > * + * {
+    margin-top: 8px;
+  }
+`
+
+const ToolBarWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 4px 8px;
+`
+
+const QuestCardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow-y: scroll;
   padding: 4px 8px;
 
   & > * + * {
@@ -15,20 +36,79 @@ const Container = styled.div`
 
 const DEFAULT_LANG = 'ja-JP'
 
+const useSearch = () => {
+  const [inputValue, setInputValue] = useState<string>()
+  const throttledValue = useThrottle(inputValue, 200)
+  const keywords = throttledValue?.split(' ').map((i) => i.toUpperCase())
+  const filterString = useCallback(
+    (text: string) => {
+      if (!keywords) {
+        return true
+      }
+      return keywords.some((keyword) => text.toUpperCase().includes(keyword))
+    },
+    [keywords]
+  )
+
+  const handleChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value)
+  }, [])
+
+  const SearchInput = useCallback(
+    () => (
+      <InputGroup
+        onChange={handleChange}
+        placeholder="Search"
+        leftIcon={IconNames.SEARCH}
+        type="text"
+      />
+    ),
+    []
+  )
+
+  return {
+    filterString,
+    SearchInput,
+  }
+}
+
+const useToolBar = () => {
+  const { filterString, SearchInput } = useSearch()
+  const ToolBar = useCallback(
+    () => (
+      <ToolBarWrapper>
+        <SearchInput></SearchInput>
+      </ToolBarWrapper>
+    ),
+    [SearchInput]
+  )
+  return {
+    filterString,
+    ToolBar,
+  }
+}
+
 export const App: React.FC = () => {
   const { i18n } = useTranslation()
+  const { filterString, ToolBar } = useToolBar()
   const LANGUAGE =
     i18n.language in QuestData
       ? (i18n.language as keyof typeof QuestData)
       : DEFAULT_LANG
 
   return (
-    <Container>
-      {Object.entries(QuestData[LANGUAGE])
-        .map(([gameId, val]) => ({ gameId, ...val }))
-        .map(({ code, name, desc }) => (
-          <QuestCard code={code} name={name} desc={desc}></QuestCard>
-        ))}
-    </Container>
+    <StrictMode>
+      <Container>
+        <ToolBar></ToolBar>
+        <QuestCardWrapper>
+          {Object.entries(QuestData[LANGUAGE])
+            .map(([gameId, val]) => ({ gameId, ...val }))
+            .filter((i) => filterString(`${i.code} ${i.name} ${i.desc}`))
+            .map(({ code, name, desc }) => (
+              <QuestCard code={code} name={name} desc={desc}></QuestCard>
+            ))}
+        </QuestCardWrapper>
+      </Container>
+    </StrictMode>
   )
 }
---
+++ src/utils.ts
@@ -0,0 +1,50 @@
+import { useEffect, useRef, useState, EffectCallback } from 'react'
+
+// See react-use
+// licensed under the The Unlicense
+// https://github.com/streamich/react-use
+
+const useEffectOnce = (effect: EffectCallback) => {
+  useEffect(effect, [])
+}
+
+const useUnmount = (fn: () => any): void => {
+  const fnRef = useRef(fn)
+
+  // update the ref each render so if it change the newest callback will be invoked
+  fnRef.current = fn
+
+  useEffectOnce(() => () => fnRef.current())
+}
+
+export const useThrottle = <T>(value: T, ms: number = 200) => {
+  const [state, setState] = useState<T>(value)
+  const timeout = useRef<ReturnType<typeof setTimeout>>()
+  const nextValue = useRef(null) as any
+  const hasNextValue = useRef(0) as any
+
+  useEffect(() => {
+    if (!timeout.current) {
+      setState(value)
+      const timeoutCallback = () => {
+        if (hasNextValue.current) {
+          hasNextValue.current = false
+          setState(nextValue.current)
+          timeout.current = setTimeout(timeoutCallback, ms)
+        } else {
+          timeout.current = undefined
+        }
+      }
+      timeout.current = setTimeout(timeoutCallback, ms)
+    } else {
+      nextValue.current = value
+      hasNextValue.current = true
+    }
+  }, [value])
+
+  useUnmount(() => {
+    timeout.current && clearTimeout(timeout.current)
+  })
+
+  return state
+}
