61681a331726dda3ca0044da23a2b28b52fd78c3
lawvs <18554747+lawvs@users.noreply.github.com>
2021-02-05 04:33:32 +0900

    feat: add i18n

---
+++ i18n/index.ts
@@ -0,0 +1,13 @@
+import zh_CN from './zh-CN.json'
+import zh_TW from './zh-TW.json'
+import ja_JP from './ja-JP.json'
+import en_US from './en-US.json'
+import ko_KR from './ko-KR.json'
+
+export const i18nResources = {
+  'zh-CN': { translation: zh_CN },
+  'zh-TW': { translation: zh_TW },
+  'ja-JP': { translation: ja_JP },
+  'en-US': { translation: en_US },
+  'ko-KR': { translation: ko_KR },
+}
