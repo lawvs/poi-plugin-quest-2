0749cea85edd8ed741e68857c95ecafddb2d9ea0
lawvs <18554747+lawvs@users.noreply.github.com>
2021-03-09 18:39:14 +0900

    chore: update settings

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,11 +1,37 @@
 import React, { StrictMode } from 'react'
-import { Text } from '@blueprintjs/core'
-import { version as PACKAGE_VERSION } from '../package.json'
+import { AnchorButton, Text } from '@blueprintjs/core'
+import { IconNames } from '@blueprintjs/icons'
+import { version as PACKAGE_VERSION, homepage } from '../package.json'
 import { version as DATA_VERSION } from '../build/kcanotifyGamedata'
+import { usePluginTranslation } from './poi'
+import styled from 'styled-components'
 
-export const Settings = () => (
-  <StrictMode>
-    <Text>Version: {PACKAGE_VERSION}</Text>
-    <Text>Data Version: {DATA_VERSION}</Text>
-  </StrictMode>
-)
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  user-select: text;
+
+  & > * + * {
+    margin-top: 8px;
+  }
+`
+
+export const Settings = () => {
+  const { t } = usePluginTranslation()
+  return (
+    <StrictMode>
+      <Container>
+        <Text>{t('Version', { version: PACKAGE_VERSION })}</Text>
+        <Text>{t('Data Version', { version: DATA_VERSION })}</Text>
+        <AnchorButton
+          icon={IconNames.CODE}
+          rightIcon={IconNames.SHARE}
+          text={t('View source code on GitHub')}
+          href={homepage}
+          target="_blank"
+        ></AnchorButton>
+      </Container>
+    </StrictMode>
+  )
+}
