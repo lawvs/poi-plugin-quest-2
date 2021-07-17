8a8b7457204e85cd94e93c28f479f1e82c343df0
lawvs <18554747+lawvs@users.noreply.github.com>
2021-07-18 02:09:34 +0800

    chore: update data version

--- src/__tests__/kcanotifyData.spec.ts
+++ src/__tests__/kcanotifyData.spec.ts
@@ -1,5 +1,5 @@
 import { version } from '../../build/kcanotifyGamedata'
 
 test('should Kcanotify Game data version correct', () => {
-  expect(version).toMatchInlineSnapshot(`"5.1.6.0"`)
+  expect(version).toMatchInlineSnapshot(`"5.2.3.0"`)
 })
