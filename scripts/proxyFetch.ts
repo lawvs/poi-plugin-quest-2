5f52aa793fe97e4268668e0709dc6e880df7aade
lawvs <18554747+lawvs@users.noreply.github.com>
2021-07-18 04:30:29 +0800

    fix: type check

--- scripts/proxyFetch.ts
+++ scripts/proxyFetch.ts
@@ -1,22 +1,30 @@
 /* eslint-disable no-console */
 import nodeFetch from 'node-fetch'
+import type { Request, RequestInfo } from 'node-fetch'
 import type { Agent } from 'http'
 import { HttpsProxyAgent } from 'https-proxy-agent'
 
 const withAgent =
   (agent: Agent) =>
   (fetch: typeof nodeFetch): typeof nodeFetch => {
+    const isRequest = (input: RequestInfo): input is Request => {
+      return typeof input === 'object' && !('href' in input)
+    }
     return new Proxy(fetch, {
-      apply(target, thisArg, argArray) {
-        if (typeof argArray[0] === 'object' && argArray[0].href !== 'string') {
+      apply(target, thisArg: unknown, argArray: Parameters<typeof nodeFetch>) {
+        if (isRequest(argArray[0])) {
           // Request
-          argArray[0].agent = agent
+          if (!argArray[0].agent) {
+            argArray[0].agent = agent
+          }
         } else {
-          // URL or URLLike + ?Request
-          if (argArray[1] === undefined) {
+          // URL or URLLike + ?RequestInit
+          if (!argArray[1]) {
             argArray[1] = {}
           }
-          argArray[1].agent = agent
+          if (!argArray[1].agent) {
+            argArray[1].agent = agent
+          }
         }
         return target.apply(thisArg, argArray)
       },
