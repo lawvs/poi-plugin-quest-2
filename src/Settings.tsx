1807add18301918d84125c2566e4396095a9e600
lawvs <18554747+lawvs@users.noreply.github.com>
2021-10-23 05:45:22 +0800

    refactor: compat settings state

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,11 +1,11 @@
-import React, { StrictMode, useCallback } from 'react'
+import React, { StrictMode } from 'react'
 import { Button, AnchorButton, Text } from '@blueprintjs/core'
 import { IconNames } from '@blueprintjs/icons'
 import styled from 'styled-components'
 import { version as PACKAGE_VERSION, homepage } from '../package.json'
 import { version as DATA_VERSION } from '../build/kcanotifyGamedata'
 import { usePluginTranslation } from './poi'
-import { removeStorage } from './store'
+import { useRemoveStorage, StoreProvider } from './store'
 
 const Container = styled.div`
   display: flex;
@@ -18,27 +18,35 @@ const Container = styled.div`
   }
 `
 
-export const Settings = () => {
+const SettingsMain = () => {
   const { t } = usePluginTranslation()
-  const handleClearCache = useCallback(() => removeStorage(), [])
+  const removeStorage = useRemoveStorage()
   return (
-    <StrictMode>
-      <Container>
-        <Text>{t('Version', { version: PACKAGE_VERSION })}</Text>
-        <Text>{t('Data Version', { version: DATA_VERSION })}</Text>
-        <AnchorButton
-          icon={IconNames.CODE}
-          rightIcon={IconNames.SHARE}
-          text={t('View source code on GitHub')}
-          href={homepage}
-          target="_blank"
-        ></AnchorButton>
-        <Button
-          icon={IconNames.TRASH}
-          text={t('Restore defaults')}
-          onClick={handleClearCache}
-        ></Button>
-      </Container>
-    </StrictMode>
+    <>
+      <Text>{t('Version', { version: PACKAGE_VERSION })}</Text>
+      <Text>{t('Data Version', { version: DATA_VERSION })}</Text>
+      <AnchorButton
+        icon={IconNames.CODE}
+        rightIcon={IconNames.SHARE}
+        text={t('View source code on GitHub')}
+        href={homepage}
+        target="_blank"
+      ></AnchorButton>
+      <Button
+        icon={IconNames.TRASH}
+        text={t('Restore defaults')}
+        onClick={removeStorage}
+      ></Button>
+    </>
   )
 }
+
+export const Settings = () => (
+  <StrictMode>
+    <StoreProvider>
+      <Container>
+        <SettingsMain />
+      </Container>
+    </StoreProvider>
+  </StrictMode>
+)
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -4,8 +4,6 @@ import React, {
   SetStateAction,
   useCallback,
   useContext,
-  useEffect,
-  useState,
 } from 'react'
 import { IN_POI } from '../poi'
 import { ALL_TYPE_TAG, ALL_CATEGORY_TAG } from '../tags'
@@ -29,16 +27,10 @@ export type State = typeof initialState
 
 // Persist state
 const STORAGE_KEY = PACKAGE_NAME
-let onRemoveStorage: (() => void)[] = []
-export const removeStorage = () => {
-  localStorage.removeItem(STORAGE_KEY)
-  onRemoveStorage.forEach((i) => i())
-}
 
 const useStorage = (
   store: State,
   setState: (state: State) => void,
-  onRemove = () => {},
   merge = true
 ) => {
   // Load storage at mount
@@ -58,24 +50,16 @@ const useStorage = (
     const serializedStore = JSON.stringify(store)
     localStorage.setItem(STORAGE_KEY, serializedStore)
   }, [store])
-
-  useEffect(() => {
-    onRemoveStorage.push(onRemove)
-    return () => {
-      onRemoveStorage = onRemoveStorage.filter((i) => i !== onRemove)
-    }
-  })
 }
 
 const StateContext = createContext<State>(initialState)
 const SetStateContext = createContext<Dispatch<SetStateAction<State>>>(() => {})
 
-export const StoreProvider = ({ children }: { children: React.ReactChild }) => {
-  const [state, setState] = useState(initialState)
-  const onStorageRemove = useCallback(() => {
-    setState(initialState)
-  }, [])
-  useStorage(state, setState, onStorageRemove)
+const useGlobalState = createGlobalState<State>(initialState)
+
+export const StoreProvider = ({ children }: { children?: React.ReactNode }) => {
+  const [state, setState] = useGlobalState()
+  useStorage(state, setState)
   return (
     <SetStateContext.Provider value={setState}>
       <StateContext.Provider value={state}>{children}</StateContext.Provider>
@@ -91,4 +75,12 @@ export const useStore = () => {
     [setStore, store]
   )
   return { store, setStore, updateStore }
+}
+
+export const useRemoveStorage = () => {
+  const { updateStore } = useStore()
+  return () => {
+    localStorage.removeItem(STORAGE_KEY)
+    updateStore(initialState)
+  }
 }
