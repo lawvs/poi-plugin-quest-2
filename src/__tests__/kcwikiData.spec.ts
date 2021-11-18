6b78a17d5109bfc8615fd6a1e1e628f57072158e
lawvs <18554747+lawvs@users.noreply.github.com>
2021-11-18 11:23:38 +0800

    chore: snapshot kcwiki quest data version

--- src/__tests__/kcwikiData.spec.ts
+++ src/__tests__/kcwikiData.spec.ts
@@ -1,9 +1,15 @@
-import { KcwikiQuestData } from '../../build/kcQuestsData'
+import { version, KcwikiQuestData } from '../../build/kcQuestsData'
+
+test('should KcwikiQuestData Game data version correct', () => {
+  expect(version).toMatchInlineSnapshot(
+    `"b76d2cf527be9266715ae5ccdd864f006a27443f"`
+  )
+})
 
 test('should KcwikiQuestData Game data keys correct', () => {
   expect(Object.keys(KcwikiQuestData)).toMatchInlineSnapshot(`
-Array [
-  "zh-CN",
-]
-`)
+    Array [
+      "zh-CN",
+    ]
+  `)
 })
