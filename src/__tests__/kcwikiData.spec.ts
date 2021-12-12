1692b85a1424e93ffa039d685aafc06e2de2a97e
lawvs <18554747+lawvs@users.noreply.github.com>
2021-12-12 14:38:03 +0800

    chore: update kcwiki data version

--- src/__tests__/kcwikiData.spec.ts
+++ src/__tests__/kcwikiData.spec.ts
@@ -2,7 +2,7 @@ import { version, KcwikiQuestData } from '../../build/kcQuestsData'
 
 test('should KcwikiQuestData Game data version correct', () => {
   expect(version).toMatchInlineSnapshot(
-    `"004638c4fa18cf4d37e45f3a8704bffe7cdaa49e"`
+    `"5fc5ff0a78daaefe383d702873a688704f546de4"`
   )
 })
 
