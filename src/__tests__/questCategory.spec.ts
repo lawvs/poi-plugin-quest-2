bb804dd9751f392f6a9bc98dc0fa50b9bb8931f1
lawvs <18554747+lawvs@users.noreply.github.com>
2022-02-20 03:31:01 +0800

    chore: add questCategory test

---
+++ src/__tests__/questCategory.spec.ts
@@ -0,0 +1,37 @@
+import questCategory from '../../build/questCategory.json'
+import { KcwikiQuestData } from '../../build/kcQuestsData'
+import { QuestData as KcaQuestData } from '../../build/kcanotifyGamedata'
+
+describe('should questCategory correct', () => {
+  test('length', () => {
+    expect(questCategory.dailyQuest.length).toMatchInlineSnapshot(`23`)
+    expect(questCategory.weeklyQuest.length).toMatchInlineSnapshot(`17`)
+    expect(questCategory.monthlyQuest.length).toMatchInlineSnapshot(`11`)
+    expect(questCategory.quarterlyQuest.length).toMatchInlineSnapshot(`25`)
+    expect(questCategory.yearlyQuest.length).toMatchInlineSnapshot(`37`)
+    expect(questCategory.singleQuest.length).toMatchInlineSnapshot(`473`)
+  })
+
+  test('snapshot', () => {
+    const mergeData = {
+      ...KcaQuestData['zh-CN'],
+      ...KcwikiQuestData['zh-CN'],
+    }
+    const humanReadableData = Object.fromEntries(
+      Object.entries(questCategory).map(([key, val]) => [
+        key,
+        val
+          .sort((a, b) => +a - +b)
+          .map((gameId) => ({
+            gameId,
+            code: mergeData[gameId as keyof typeof mergeData]?.code,
+            name: mergeData[gameId as keyof typeof mergeData]?.name,
+          })),
+      ])
+    )
+
+    expect(humanReadableData).toMatchSnapshot({
+      singleQuest: expect.any(Array),
+    })
+  })
+})
