5e718e5e75b32285114b84d1843128e4097f4fd1
lawvs <18554747+lawvs@users.noreply.github.com>
2022-02-21 01:00:59 +0800

    chore: update tests

--- src/__tests__/kcwikiData.spec.ts
+++ src/__tests__/kcwikiData.spec.ts
@@ -1,15 +1,28 @@
 import { version, KcwikiQuestData } from '../../build/kcQuestsData'
 
-test('should KcwikiQuestData Game data version correct', () => {
-  expect(version).toMatchInlineSnapshot(
-    `"be5db381c0cca84ede0166eb8c582d19afe0aec1"`
-  )
-})
+describe('should version correct', () => {
+  test('should KcwikiQuestData Game data version correct', () => {
+    expect(version).toMatchInlineSnapshot(
+      `"9c5a15512d59eaad02e09fe717defba581c4e4a3"`
+    )
+  })
 
-test('should KcwikiQuestData Game data keys correct', () => {
-  expect(Object.keys(KcwikiQuestData)).toMatchInlineSnapshot(`
+  test('should KcwikiQuestData Game data keys correct', () => {
+    expect(Object.keys(KcwikiQuestData)).toMatchInlineSnapshot(`
     Array [
       "zh-CN",
     ]
   `)
+  })
+})
+
+describe('should format correct', () => {
+  test('key format', () => {
+    Object.keys(KcwikiQuestData['zh-CN']).forEach((key) => {
+      // gameId should not extra space
+      expect(key.trim()).toEqual(key)
+      // gameId should be number
+      expect(String(+key)).toEqual(key)
+    })
+  })
 })
