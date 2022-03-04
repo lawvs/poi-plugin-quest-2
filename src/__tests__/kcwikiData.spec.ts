a68b8cb838d7c03f161c97d59e3c2f69b7ea3043
lawvs <18554747+lawvs@users.noreply.github.com>
2022-03-05 03:08:43 +0800

    chore: update data

--- src/__tests__/kcwikiData.spec.ts
+++ src/__tests__/kcwikiData.spec.ts
@@ -3,16 +3,16 @@ import { version, KcwikiQuestData } from '../../build/kcQuestsData'
 describe('should version correct', () => {
   test('should KcwikiQuestData Game data version correct', () => {
     expect(version).toMatchInlineSnapshot(
-      `"9c5a15512d59eaad02e09fe717defba581c4e4a3"`
+      `"f4954599e699417bca2f855c1b67c695837eb5c5"`
     )
   })
 
   test('should KcwikiQuestData Game data keys correct', () => {
     expect(Object.keys(KcwikiQuestData)).toMatchInlineSnapshot(`
-    Array [
-      "zh-CN",
-    ]
-  `)
+          Array [
+            "zh-CN",
+          ]
+      `)
   })
 })
 
