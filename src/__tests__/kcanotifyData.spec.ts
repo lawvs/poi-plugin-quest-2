8ef3ce97b361ca076213c120e66ff98279c196e8
lawvs <18554747+lawvs@users.noreply.github.com>
2022-10-29 11:20:21 +0800

    chore: update data

--- src/__tests__/kcanotifyData.spec.ts
+++ src/__tests__/kcanotifyData.spec.ts
@@ -1,7 +1,7 @@
 import { version, QuestData } from '../../build/kcanotifyGamedata'
 
 test('should Kcanotify Game data version correct', () => {
-  expect(version).toMatchInlineSnapshot(`"2022091101"`)
+  expect(version).toMatchInlineSnapshot(`"2022102904"`)
 })
 
 test('should Kcanotify Game data keys correct', () => {
